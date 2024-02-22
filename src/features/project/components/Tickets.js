2668c19161893e09a6c36e7df46faaf3e49ad5b9
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-02-22 13:12:52 -0300

    Display project tickets table based on request data

--- src/features/project/components/Tickets.js
+++ src/features/project/components/Tickets.js
@@ -5,77 +5,35 @@ import TitleCard from '../../../components/Cards/TitleCard';
 import { showNotification } from '../../common/headerSlice';
 import axios from 'axios';
 import { useNavigate, useParams } from 'react-router-dom';
+import { parse, formatDistanceToNow } from 'date-fns';
 
-const TopSideButtons = () => {
-  const dispatch = useDispatch();
-
-  const addNewTeamMember = () => {
-    dispatch(
-      showNotification({ message: 'Add New Member clicked', status: 1 })
-    );
-  };
-
-  return (
-    <div className='inline-block float-right'>
-      <button
-        className='btn px-6 btn-sm normal-case btn-primary'
-        onClick={() => addNewTeamMember()}
-      >
-        Invite New
-      </button>
-    </div>
-  );
-};
-
-function RoleSelect({ role, userId }) {
-  const { id } = useParams();
-  const handleRoleSelect = (e) => {
-    const role = e.target.value;
-    axios.post(`/api/projects/${id}/changeRole/${userId}/${role}`);
-  };
-  return (
-    <>
-      <select
-        className='select select-bordered select-sm w-max'
-        defaultValue={role}
-        onChange={handleRoleSelect}
-      >
-        <option value={'Developer'}>Developer</option>
-        <option value={'Manager'}>Manager</option>
-        <option value={'Submitter'}>Submitter</option>
-      </select>
-    </>
-  );
-}
 
 function Tickets() {
   const [members, setMembers] = useState([]);
   const navigate = useNavigate();
+  const { id } = useParams();
 
   useEffect(() => {
-    async function fetchUsers() {
-      const res = await axios.get('/api/users', {});
-      const users = res.data.data;
-      setMembers(users);
+    async function fetchTickets() {
+      const res = await axios.get(`/api/projects/${id}/tickets`, {});
+      const tickets = res.data.data;
+      setMembers(tickets);
     }
-    fetchUsers();
+    fetchTickets();
   }, []);
 
-    const getPriorityComponent = (pr) => {
-      if (pr === 'High')
-        return <div className='badge badge-error'>{pr}</div>;
-      if (pr === 'Medium') return <div className='badge badge-primary'>{pr}</div>;
-      if (pr === 'Low')
-        return <div className='badge badge-ghost'>{pr}</div>;
-      else return <div className='badge badge-ghost'>{pr}</div>;
-    };
+  const getPriorityComponent = (pr) => {
+    if (pr === 'High') return <div className='badge badge-accent'>{pr}</div>;
+    if (pr === 'Medium') return <div className='badge badge-primary'>{pr}</div>;
+    if (pr === 'Low') return <div className='badge badge-ghost'>{pr}</div>;
+    else return <div className='badge badge-ghost'>{pr}</div>;
+  };
 
-    const getDifficultyComponent = getPriorityComponent;
+  const getDifficultyComponent = getPriorityComponent;
 
-
-    const goRouteId = (id) => {
-      navigate(`/app/tickets/${id}`);
-    };
+  const goRouteId = (id) => {
+    navigate(`/app/tickets/${id}`);
+  };
 
   return (
     <>
@@ -96,35 +54,34 @@ function Tickets() {
           <tbody>
             {members.map((l, k) => {
               return (
-                <tr key={k}
-                className='hover cursor-pointer'
-                    onClick={() => {
-                      goRouteId(l.id);
-                    }}>
+                <tr
+                  key={k}
+                  className='hover cursor-pointer'
+                  onClick={() => {
+                    goRouteId(l.id);
+                  }}
+                >
+                  <td>{l.id}</td>
                   <td>
-                    l.id
+                    <div className='font-bold'>{l.title}</div>
                   </td>
+                  <td>{l.type}</td>
+                  <td>{getPriorityComponent(l.priority)}</td>
+                  <td>{getDifficultyComponent(l.difficulty)}</td>
+                  <td>{l.assignedTo.name}</td>
+                  <td>{l.status}</td>
                   <td>
-                    <div className='font-bold'>l.title</div>
+                    {formatDistanceToNow(
+                      parse(l.updatedAt, 'dd MMM yy HH:mm', new Date()),
+                      { addSuffix: true }
+                    )}
                   </td>
-                  <td>l.type</td>
-                  <td>getPriorityComponent(l.priority)</td>
-                  <td>getDifficultyComponent(l.difficulty)</td>
-                  <td>l.assignedTo</td>
-                  <td>l.status</td>
-                  <td>--
-                    {/* formatDistanceToNow(
-                        parse(l.updatedAt, 'dd MMM yy HH:mm', new Date()),
-                        { addSuffix: true }
-                      ) */}
-                      </td>
                 </tr>
               );
             })}
           </tbody>
         </table>
       </div>
-      {/* </TitleCard> */}
     </>
   );
 }
