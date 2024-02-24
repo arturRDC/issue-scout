a5b188f06c1fbae9986352d0db8facf12009358a
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2024-02-24 18:24:16 -0300

    Update login and register page styles and content

--- src/features/user/components/TemplatePointers.js
+++ src/features/user/components/TemplatePointers.js
@@ -1,14 +1,30 @@
-function TemplatePointers(){
-    return(
-        <>
-         <h1 className="text-2xl mt-8 font-bold">Admin Dashboard Starter Kit</h1>
-          <p className="py-2 mt-4">✓ <span className="font-semibold">Light/dark</span> mode toggle</p>
-          <p className="py-2 ">✓ <span className="font-semibold">Redux toolkit</span> and other utility libraries configured</p>
-          <p className="py-2">✓ <span className="font-semibold">Calendar, Modal, Sidebar </span> components</p>
-          <p className="py-2  ">✓ User-friendly <span className="font-semibold">documentation</span></p>
-          <p className="py-2  mb-4">✓ <span className="font-semibold">Daisy UI</span> components, <span className="font-semibold">Tailwind CSS</span> support</p>
-        </>
-    )
+function TemplatePointers() {
+  return (
+    <>
+      <h1 className='text-2xl mt-8 font-bold'>IssueScout Bug Tracker</h1>
+      <p className='py-2 mt-4'>
+        ✓ <span className='font-semibold'>Project Management:</span> Create and
+        manage multiple projects to track bugs and issues.
+      </p>
+      <p className='py-2'>
+        ✓ <span className='font-semibold'>Light/dark</span> mode
+      </p>
+      <p className='py-2'>
+        ✓ <span className='font-semibold'>Role-based Access Control:</span> Four
+        user roles with distinct permissions and functionalities.
+      </p>
+      <p className='py-2'>
+        ✓ <span className='font-semibold'>Ticket Management:</span> Easily
+        create, update, and assign tickets to track and resolve issues
+        efficiently.
+      </p>
+      <p className='py-2  '>
+        ✓ Extra features like{' '}
+        <span className='font-semibold'>version history </span> and a{' '}
+        <span className='font-semibold'> comment system </span>
+      </p>
+    </>
+  );
 }
 
-export default TemplatePointers
+export default TemplatePointers;
